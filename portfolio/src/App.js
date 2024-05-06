0d045acce75a190dc61031c1b080af6ac0cdc7b7
BeSingularTeam <besingulardemo@gmail.com>
2024-05-07 00:41:39 +0530

    first commit

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import Sidebar from "./Components/Layout/Sidebar";
+
+function App() {
+  return(
+    <div className="App">
+      <Sidebar/>
+    </div>
+
+  );
+  
+}
+
+export default App;
---
+++ portfolio/src/Components/Layout/Sidebar.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import HomePage from '../../Pages/Home/HomePage';
+import { AiOutlineDoubleLeft, AiOutlineDoubleRight } from 'react-icons/ai';
+import './Sidebar.css';
+
+const Sidebar = () => {
+    const [toggle, setToggle] = useState(true); // Corrected useState usage
+
+    // Change toggle
+    const handleToggle = () => {
+        setToggle(!toggle);
+    };
+
+    return (
+        <div>
+            <div className="sidebar-section">
+                <div className={toggle ? "sidebar-toggle sidebar": "sidebar"}>
+                    <div className="sidebar-toggle-icons">
+                        <p onClick={handleToggle}>
+                            {
+                                toggle
+                                    ? <AiOutlineDoubleLeft size={30} />
+                                    : <AiOutlineDoubleRight size={30} />
+                            }
+                        </p>
+                    </div>
+                </div>
+                <div className="container">
+                    <HomePage />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
