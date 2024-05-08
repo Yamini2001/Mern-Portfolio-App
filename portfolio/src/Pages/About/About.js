ff1e9a8306ca29adc808a25b18ca9ee364c77402
BeSingularTeam <besingulardemo@gmail.com>
2024-05-09 00:59:10 +0530

    About

--- portfolio/src/Pages/About/About.js
+++ portfolio/src/Pages/About/About.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './About.css';
+import YaminiImage from '../About/images/Yamini.jpeg';
 
 const About= () => {
   return (
     <div>
-      
+      <div className="about">
+        <div className="row">
+            <div className="col-md-6 col-xl-6 col-lg-6 about-img img">
+                <img src={YaminiImage} alt="profile-pic"/>
+            </div>
+            <div className="col-md-6">
+                Content
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
