d126cabab7d2c54913e72664eed4922e34010eae
BeSingularTeam <besingulardemo@gmail.com>
2024-05-08 23:42:40 +0530

    commitsecond

--- portfolio/src/Pages/Home/HomePage.js
+++ portfolio/src/Pages/Home/HomePage.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import './HomePage.css';
-
+import Typewriter from 'typewriter-effect';
+import YaminiResume from '../Home/images/Yamini _Resume.pdf';
 const HomePage = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <div className="container-fluid home-container">
+      <div className="moving-shape"></div>
+            <div className="moving-code-snippet">
+        <div className="container home-content h1">
+          <h1>Hi I'm a</h1>
+          <h2>
+            <Typewriter
+  options={{
+    strings: ['FullStack Developer!', 'MERN Stack Developer!','React JS Developer!'],
+    autoStart: true,
+    loop: true,
+  }} 
+/>
+    </h2>
+      <div className="home-buttons">
+         <button className='btn btn-hire'>Hire Me</button>
+          <a className='btn btn-cv' href={YaminiResume} download="Yamini's_Resume.pdf">My Resume</a>
+
+       </div>
+       </div>
+      </div>
+    </div>
     </div>
   )
 }
