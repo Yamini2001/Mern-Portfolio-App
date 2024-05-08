151cf3401d2919a3d1e27b1a298f7dee192ed538
BeSingularTeam <besingulardemo@gmail.com>
2024-05-09 00:40:49 +0530

    update1

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,11 @@
 import Sidebar from "./Components/Layout/Sidebar";
+import About from "./Pages/About/About";
 
 function App() {
   return(
     <div className="App">
       <Sidebar/>
+      <About/> 
     </div>
 
   );
--- portfolio/src/Pages/Home/HomePage.js
+++ portfolio/src/Pages/Home/HomePage.js
@@ -9,8 +9,8 @@ const HomePage = () => {
       <div className="moving-shape"></div>
             <div className="moving-code-snippet">
         <div className="container home-content h1">
-          <h1>Hi I'm a</h1>
-          <h2>
+          <h2>Hi 👋 I'm a</h2>
+          <h1>
             <Typewriter
   options={{
     strings: ['FullStack Developer!', 'MERN Stack Developer!','React JS Developer!'],
@@ -18,7 +18,7 @@ const HomePage = () => {
     loop: true,
   }} 
 />
-    </h2>
+    </h1>
       <div className="home-buttons">
          <button className='btn btn-hire'>Hire Me</button>
           <a className='btn btn-cv' href={YaminiResume} download="Yamini's_Resume.pdf">My Resume</a>
